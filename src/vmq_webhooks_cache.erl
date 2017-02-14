3c67f9db57b55ab16116efd1af3807dced70bb80
Lars Hesel Christensen <lars@larshesel.dk>
2017-02-14 11:03:09 +0100

    Add webhooks cache

---
+++ src/vmq_webhooks_cache.erl
@@ -0,0 +1,104 @@
+%% Copyright 2017 Erlio GmbH Basel Switzerland (http://erl.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%% @doc A cache for `vmq_webhooks` used to cache the hooks
+%% `auth_on_register`, `auth_on_publish` and `auth_on_subscribe` as
+%% authentication data is often static.
+-module(vmq_webhooks_cache).
+
+-export([
+         new/0
+        ,reset_stats/0
+        ,lookup/3
+        ,insert/4
+        ,stats/0
+        ,purge_all/0
+        ]).
+
+-define(CACHE, vmq_webhooks_cache).
+-define(STATS, vmq_webhooks_cache_stats).
+
+%% API
+new() ->
+    ets:new(?CACHE, [public, bag, named_table, {read_concurrency, true}]),
+    ets:new(?STATS, [public, ordered_set, named_table, {write_concurrency, true}]),
+    ets:insert(?STATS, {hits, 0}),
+    ets:insert(?STATS, {misses, 0}),
+    ok.
+
+reset_stats() ->
+    ets:insert(?STATS, {hits, 0}),
+    ets:insert(?STATS, {misses, 0}).
+
+purge_all() ->
+    ets:delete_all_objects(?CACHE),
+    reset_stats().    
+
+lookup(Endpoint, Hook, Args) ->
+    case lookup_(Endpoint, Hook, Args) of
+        not_found ->
+            miss(),
+            not_found;
+        Val ->
+            hit(),
+            Val
+    end.
+
+insert(Endpoint, Hook, Args, ExpiryInSecs) ->
+    SubscriberId =
+        {proplists:get_value(mountpoint, Args),
+         proplists:get_value(client_id, Args)},
+    ExpirationTs = ts_from_now(ExpiryInSecs),
+    %% Remove the payload from cache, as it doesn't make sense to
+    %% cache that.
+    Key = {Endpoint, Hook, lists:keydelete(payload, 1, Args)},
+    Row = {Key, SubscriberId, ExpirationTs},
+    true = ets:insert(?CACHE, Row),
+    ok.
+
+%% internal functions.
+lookup_(Endpoint, Hook, Args) ->
+    %% The payload is not part of the key, so we remove it.
+    Key = {Endpoint, Hook, lists:keydelete(payload, 1, Args)},
+    case ets:lookup(?CACHE, Key) of
+        [] ->
+            not_found;
+        [{{_EP,_H,_Args},_Sid,ExpirationTs}] = Val ->
+            case expired(ExpirationTs) of
+                true ->
+                    ets:delete(?CACHE, Key),
+                    not_found;
+                false ->
+                    Val
+            end
+    end.
+
+miss() ->
+    ets:update_counter(?STATS, misses, 1, {misses, 0}).
+
+hit() ->
+    ets:update_counter(?STATS, hits, 1, {hits, 0}).
+
+expired(ExpirationTs) ->
+    ExpirationTs < erlang:system_time(second).
+
+ts_from_now(MaxAge) ->
+    erlang:system_time(second) + MaxAge.
+
+stats() ->
+    [{_,Hits}] = ets:lookup(?STATS, hits),
+    [{_,Misses}] = ets:lookup(?STATS, misses),
+    #{hits => Hits,
+      misses => Misses,
+      entries => ets:info(?CACHE, size)}.
--- src/vmq_webhooks_cli.erl
+++ src/vmq_webhooks_cli.erl
@@ -1,4 +1,4 @@
-%% Copyright 2016 Erlio GmbH Basel Switzerland (http://erl.io)
+%% Copyright 2017 Erlio GmbH Basel Switzerland (http://erl.io)
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -20,7 +20,26 @@ register_cli() ->
     register_cli_usage(),
     status_cmd(),
     register_cmd(),
+    cache_stats_cmd(),
     deregister_cmd().
+
+cache_stats_cmd() ->
+    Cmd = ["vmq-admin", "webhooks", "cache", "stats"],
+    FlagSpecs = [{reset, [{longname, "reset"}]}],
+    Callback =
+        fun(_, [], []) ->
+                Table =
+                    [[{stat, Stat}, {value, Value}] ||
+                         {Stat, Value} <- maps:to_list(vmq_webhooks_cache:stats())],
+                [clique_status:table(Table)];
+           (_, [], [{reset, _ResetFlag}]) ->
+                vmq_webhooks_cache:reset_stats(),
+                [clique_status:text("Done")];
+           (_, _, _) ->
+                Text = clique_status:text(cache_usage()),
+                [clique_status:alert([Text])]
+        end,
+    clique:register_command(Cmd, [], FlagSpecs, Callback).
 
 status_cmd() ->
     Cmd = ["vmq-admin", "webhooks", "status"],
@@ -31,7 +50,10 @@ status_cmd() ->
                       {base64payload, b64opt(Opts)}] ||
                         {Hook, Endpoints} <- vmq_webhooks_plugin:all_hooks(),
                         {Endpoint, Opts} <- Endpoints],
-                [clique_status:table(Table)]
+                [clique_status:table(Table)];
+           (_, _, _) ->
+                Text = clique_status:text(webhooks_usage()),
+                [clique_status:alert([Text])]
         end,
     clique:register_command(Cmd, [], [], Callback).
 
@@ -134,7 +156,8 @@ register_cli_usage() ->
     clique:register_usage(["vmq-admin", "webhooks"], webhooks_usage()),
     clique:register_usage(["vmq-admin", "webhooks", "register"], register_usage()),
     clique:register_usage(["vmq-admin", "webhooks", "deregister"], deregister_usage()),
-    clique:register_usage(["vmq-admin", "webhooks", "status"], status_usage()).
+    clique:register_usage(["vmq-admin", "webhooks", "status"], status_usage()),
+    clique:register_usage(["vmq-admin", "webhooks", "cache"], cache_usage()).
 
 webhooks_usage() ->
     ["vmq-admin webhooks <sub-command>\n\n",
@@ -142,7 +165,8 @@ webhooks_usage() ->
      "  Sub-commands:\n",
      "    status      Show the status of registered webhooks\n",
      "    register    Register a webhook\n",
-     "    deregister  Deregister a webhook\n\n",
+     "    deregister  Deregister a webhook\n",
+     "    cache       Manage the webhooks cache\n\n",
      "  Use --help after a sub-command for more details.\n"
     ].
 
@@ -165,4 +189,12 @@ status_usage() ->
     ["vmq-admin webhooks status\n\n",
      "  Shows the information of the registered webhooks.",
      "\n\n"
+    ].
+
+cache_usage() ->
+    ["vmq-admin webhooks cache\n\n",
+     "  Manage the webhooks cache."
+     "\n\n",
+     "  Sub-commands:\n",
+     "    stats       Show statistics about the cache\n"
     ].
